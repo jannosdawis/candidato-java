b0997a0b5b74ffed82fda1ea61d149b72cef92f3
jannosdawis <jdawis@outlook.com>
2020-05-13 20:35:08 -0300

    Leitura da escolaridade

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,5 +1,19 @@
 package aplicacao;
+
+import java.util.Scanner;
+
 public class programa {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int opcao;
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		opcao = sc.nextInt();
+		
+		sc.close();
 	}
 }
