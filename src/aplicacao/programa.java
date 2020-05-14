b55602023048023b425173983efc1ba6d8287d44
jannosdawis <jdawis@outlook.com>
2020-05-13 21:22:38 -0300

    Leitura dos outros dados

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class programa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int opcao;
+		int opcao, experiencia;
+		char disponibilidade, habilitacao;
+		
 		System.out.println("Qual sua escolaridade?");
 		System.out.println("1) Ensino Fundamental");
 		System.out.println("2) Ensino Médio");
@@ -13,7 +15,13 @@ public class programa {
 		System.out.println("4) Pós-graduação");
 		System.out.print("Digite uma opção: ");
 		opcao = sc.nextInt();
-		
+		System.out.println("");
+		System.out.print("Você tem quantos anos de experiência profissional? ");
+		experiencia = sc.nextInt();
+		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
+		disponibilidade = sc.next().charAt(0);
+		System.out.print("Você tem habilitação de motorista (S/N)? ");
+		habilitacao = sc.next().charAt(0);
 		sc.close();
 	}
 }
